c7dac86e2e78bc09ee98b6b65873811435beacf0
Rudra Patel <3rudrap1@gmail.com>
2025-01-09 15:55:35 -0800

    added sentiment model to calculate sentiment score

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 
 from scraping.webScraping import scrape_headlines
 from models.stock import Stock
-
+from models.sentimentModel import sentiment_analysis
 
 def main():
     headlines = scrape_headlines()
     for stock in headlines:
+        stock.sentiment_score = sentiment_analysis(stock.headline)
         print(stock)
 
 
--- models/sentimentModel.py
+++ models/sentimentModel.py
@@ -1,4 +1,8 @@
+from transformers import pipeline
+
+pipe = pipeline("text-classification", model="mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
 
 
-def sentiment_analysis():
-    hi
+def sentiment_analysis(headline):
+    result = pipe(headline)
+    return result[0]['score']
