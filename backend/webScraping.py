2f5c412356fb1db30b818d583607be4bb8a3f9f7
Rudra Patel <rudrap31@students.cs.ubc.ca>
2025-02-17 19:32:24 -0800

    fixed web scraping request from getting blocked

--- backend/webScraping.py
+++ backend/webScraping.py
@@ -6,7 +6,10 @@ from stock import Stock
 
 def scrape_headlines():
     URL = "https://finance.yahoo.com/news/" # Use yahoo to scrape news
-    r = requests.get(URL)
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+    r = requests.get(URL, headers=headers)
     soup = BeautifulSoup(r.content, 'html.parser')
 
     headlines = soup.find('ul', class_ = 'story-items pb-small yf-o0ewvr')
